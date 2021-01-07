6614b9eccea07f9c4a0a571b2fc992108d1a87ae
김경훈 <kidi052812@gmail.com>
2021-01-07 20:38:48 +0900

    [INIT] : create-react-app

--- backend/index.js
+++ backend/index.js
@@ -13,14 +13,15 @@ const { User } = require("./models/User")
 // app.use() : 서버를 프로그래밍할 때 꼭 필요한 모듈들을 사용하는 것, middleware 역할
 app.use(bodyParser.urlencoded({extended: true})) // application/x-www-form-urlencoded을 분석해서 가져옴.
 app.use(bodyParser.json()) // application/json을 분석해서 가져옴.
-
 app.use(cookieParser());
 
 const mongoose = require('mongoose')
+
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false // 써 줘야 에러가 안난다고 함.
 }).then(() => console.log("MongoDB Connected...")) // 연결이 잘되었는지, 안되었는지 확인
   .catch(err => console.log(err)) // 에러가 생길 때 보여줌.
+
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
