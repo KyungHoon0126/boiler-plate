70594e23688e1a079a59a6908c1b80ce76898c3f
김경훈 <kidi052812@gmail.com>
2021-01-07 19:29:25 +0900

    [ADD] : Complete Backend

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,17 +8,24 @@ let auth = (req, res, next) => {
 
     // 토큰을 복호화 한후 유저를 찾는다.
     User.findByToken(token, (err, user) => {
-        if(err) throw err;
-        if(!user) return res.json({ isAuth: false, error: true })
+        if(err) {
+            throw err;
+        } 
+
+        if(!user) {
+            console.log(token)
+            console.log("auth : !user")
+            return res.json({ isAuth: false, error: true })
+        } 
 
         req.token = token;
         req.user = user;
         next(); // next()를 안해주면 middleware에서 갇힌다.
-    })
+    });
 
     // 유저가 있으면 인증 OK
 
     // 유저가 없으면 인증 NO
-}
+};
 
 module.exports = { auth };
